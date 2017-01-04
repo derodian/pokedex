1abb4a0319eed8c0775058f1710a607c546f7405
Derodian <hitz@hitzs-imac.home>
2017-01-04 02:17:18 -0600

    CollectionView loading complete

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Hitz on 1/4/17.
+//  Copyright © 2017 Hitanshu Bhakta. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
