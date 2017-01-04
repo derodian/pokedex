26a21bee9b1743d275bdd0fdca5636835e064555
Derodian <hitz@hitzs-imac.home>
2017-01-04 00:22:43 -0600

    Created Collection View Cell & Background Image

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Hitz on 1/3/17.
+//  Copyright © 2017 Hitanshu Bhakta. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
